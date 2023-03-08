4f839410127fb360e6f5ed3a578a2da175168243
tarki2206 <sasha100500@yahoo.com>
2023-03-08 13:40:32 +0800

    u5!!

--- yatube/posts/migrations/0001_initial.py
+++ yatube/posts/migrations/0001_initial.py
@@ -17,7 +17,10 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Group',
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.AutoField(auto_created=True,
+                                        primary_key=True,
+                                        serialize=False,
+                                        verbose_name='ID')),
                 ('title', models.CharField(max_length=200)),
                 ('slug', models.SlugField(unique=True)),
                 ('description', models.TextField()),
@@ -26,11 +29,19 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Post',
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.AutoField(auto_created=True,
+                                        primary_key=True,
+                                        serialize=False,
+                                        verbose_name='ID')),
                 ('text', models.TextField()),
                 ('pub_date', models.DateTimeField(auto_now_add=True)),
-                ('author', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='posts', to=settings.AUTH_USER_MODEL)),
-                ('group', models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, to='posts.Group')),
+                ('author', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE,
+                                             related_name='posts',
+                                             to=settings.AUTH_USER_MODEL)),
+                ('group', models.ForeignKey(blank=True,
+                                            null=True,
+                                            on_delete=django.db.models.deletion.CASCADE,
+                                            to='posts.Group')),
             ],
             options={
                 'ordering': ['-pub_date'],
--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -16,8 +16,14 @@ class Group(models.Model):
 class Post(models.Model):
     text = models.TextField()
     pub_date = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
-    group = models.ForeignKey(Group, blank=True, null=True, on_delete=models.CASCADE, related_name='posts')
+    author = models.ForeignKey(User,
+                               on_delete=models.CASCADE,
+                               related_name='posts')
+    group = models.ForeignKey(Group,
+                              blank=True,
+                              null=True,
+                              on_delete=models.CASCADE,
+                              related_name='posts')
 
     class Meta:
         ordering = ['-pub_date']
